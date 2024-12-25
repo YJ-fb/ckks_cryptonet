8011a19a86717bc127bb8ccd68c4a2190f1fb39d
Jing <131523638+YJ-fb@users.noreply.github.com>
2024-12-25 14:10:19 +0800

    Update Experiment.py

--- modules/Experiment.py
+++ modules/Experiment.py
@@ -36,7 +36,7 @@ class Experiment:
     def plotResults(self, scan_object = None, analyze_file = None):
         analyze_object = None
         
-        if(scan_object != None):
+        if(scan_object is not None):  # 如果提供了scan对象，则创建一个Analyze对象
             analyze_object = talos.Analyze(scan_object)         
         
         if(analyze_file != None):
